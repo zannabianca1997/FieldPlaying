594963cf1dbdbcc3e039a34df376089ea7fb3273
zannabianca1997 <zannabianca199712@gmail.com>
2016-03-08 15:45:43 +0100

    Base code

---
+++ main.py
@@ -0,0 +1,60 @@
+from read_scene import read_scene
+
+print("Loading setups...")
+#eventual general setups
+print("Loading scene...")
+scene = read_scene("ch_dist_1.json")
+
+import numpy as np
+print("Creating base coordinate matrix...")
+x = np.linspace(scene.graph_setup.x.min, scene.graph_setup.x.max, scene.graph_setup.x.div)
+y = np.linspace(scene.graph_setup.y.min, scene.graph_setup.y.max, scene.graph_setup.y.div)
+X, Y = np.meshgrid(x, y)
+data_shape=X.shape
+print("Data shape setted to {}".format(data_shape))
+
+print("Creating scene...")
+Charge = scene.paint(X, Y)
+
+print("Creating starting zero fields...")
+E_x = np.zeros(data_shape)
+E_y = np.zeros(data_shape) #all separate
+P = np.zeros(data_shape)
+
+print("Creating distance matrix...")
+
+
+def dist_matrix(dimx, dimy):
+    print("    Creating duble base coordinate matrix...")
+    x = np.arange(-dimx, dimx + 1)
+    y = np.arange(-dimy, dimy + 1)
+    X, Y = np.meshgrid(x, y)
+    return np.sqrt(X ** 2 + Y ** 2)
+
+D_matrix = dist_matrix(*data_shape) * scene.graph_setup.prec #distance, cell [datashape] is 0
+
+def blit(dest, src, loc): #blit function
+    pos = [i if i >= 0 else None for i in loc]
+    neg = [-i if i < 0 else None for i in loc]
+    target = dest[[slice(i,None) for i in pos]]
+    src = src[[slice(i, j) for i,j in zip(neg, target.shape)]]
+    target[[slice(None, i) for i in src.shape]] = src
+    return dest
+
+import plotly
+plotly.offline.plot(plotly.graph_objs.Data([
+    plotly.graph_objs.Contour(x=x, y=y, z=(Charge / (scene.graph_setup.prec**2) ),
+                           contours=dict(
+                                coloring='heatmap'
+                           ),
+                           colorbar=dict(
+                                thickness=25,
+                                thicknessmode='pixels',
+                                len=0.5,
+                                lenmode='fraction',
+                                outlinewidth=0,
+                                ticksuffix = "C/m^2"
+                            ))]), filename='TestFile.html')
+
+
+
---
+++ read_scene.py
@@ -0,0 +1,68 @@
+import json
+import numpy as np
+
+
+class bounds:
+    def __init__(self, min_val, max_val, prec):
+        self.min = min_val
+        self.max = max_val
+        self.div = int((max_val - min_val) // prec)
+
+
+class graphdata:
+    def __init__(self, datas):
+        self.prec = datas["prec"]
+        self.x = bounds(datas["xmin"], datas["xmax"], self.prec)
+        self.y = bounds(datas["ymin"], datas["ymax"], self.prec)
+
+
+class vect_interpreter:
+
+    def paint_circle(X, Y, data, graphdata):
+        center = data["center"]
+        dists_sqr = (X - center[0]) ** 2 + (Y - center[1]) ** 2 #all this are matrices
+        return dists_sqr < (data["radius"]**2) # here is the circle
+
+    __shapes_types = {"circle": paint_circle}
+
+    def paint_uniform(X, Y, new_field, data, graphdata):
+        if "total" in data: #se ha specificato il totale
+            cell_charge = data["total"] / np.sum(new_field) #divide sulle celle della figura
+        elif "density" in data: # se ha specificato la densità
+            cell_charge = data["density"] * (graphdata.prec ** 2) #densità per area della cella
+        else:
+            raise Exception("You must specify total or density for uniform charge")
+        return np.full(new_field.shape, cell_charge) #campo costante
+
+    __charges_fill = {"uniform": paint_uniform}
+
+    def paint(self, content, graphdata, X, Y):
+        field = np.zeros(X.shape)
+        for shape in content:
+            new_field = self.__shapes_types[shape["shape"]["type"]](X, Y, shape["shape"], graphdata) #fill the field with the shape
+            new_field = new_field * \
+                        self.__charges_fill[shape["charge"]["type"]]\
+                            (X, Y, new_field, shape["charge"], graphdata) #charge the shape
+            field += new_field
+        return field
+
+class charge_field:
+    __paint_interpreters = {"vectorial":vect_interpreter}
+
+    def __init__(self, datas, graph_setup):
+        self.type = datas["type"]
+        self.content = datas["content"]
+        self.interpreter = self.__paint_interpreters[self.type]() #crea l'interprete
+        self.graph_setup = graph_setup
+
+    def paint(self, *indexes):
+        return self.interpreter.paint(self.content, self.graph_setup, *indexes)
+
+
+
+def read_scene(filename):
+    with open(filename) as f:
+        set_up = json.loads(f.read())
+    graph_setup = graphdata(set_up["graphdata"])
+    charges = charge_field(set_up["charges"], graph_setup) #gli servono i dati nel caso di una datamap
+    return charges
