7dfde8721945fe4f9ad0847b72eb42ad0da3273f
zannabianca1997 <zannabianca199712@gmail.com>
2016-03-09 17:06:46 +0100

    Created slices array, potential calc

--- main.py
+++ main.py
@@ -11,49 +11,78 @@ x = np.linspace(scene.graph_setup.x.min, scene.graph_setup.x.max, scene.graph_se
 y = np.linspace(scene.graph_setup.y.min, scene.graph_setup.y.max, scene.graph_setup.y.div)
 X, Y = np.meshgrid(x, y)
 data_shape=X.shape
-print("Data shape setted to {}".format(data_shape))
+cellnum= data_shape[0]*data_shape[1]
+print("Data shape setted to {}, simulating {} cells".format(data_shape, cellnum))
 
 print("Creating scene...")
 Charge = scene.paint(X, Y)
+
+print("Creating distance matrix...")
+
+def sqr_dist_matrix(dimx, dimy):
+    x = np.arange(-dimx, dimx + 1) #+1 is for safety.. i doubt its usefullness
+    y = np.arange(-dimy, dimy + 1)
+    X, Y = np.meshgrid(x, y)
+    return X ** 2 + Y ** 2
+
+print("    Creating duble base distance matrices...")
+int_D_matrix = sqr_dist_matrix(*data_shape) #squared int distance, D_matrix[datashape] is 0
+sqr_D_matrix = int_D_matrix * (scene.graph_setup.prec**2) #distance squared
+D_matrix = np.sqrt(int_D_matrix) * scene.graph_setup.prec #distance, i multiply here for precision sake
+
+
+print("    Preinverting and multiplying by k...")
+k = 1/(4*np.pi*8.86e-12)
+E_matrix = k / sqr_D_matrix #electric field
+P_matrix = k / D_matrix     #electric potential
+print("    Erasing infinite center value...")
+E_matrix = np.nan_to_num(E_matrix)
+P_matrix = np.nan_to_num(P_matrix)
+
+print("    Creating slicing arrays...")
+#slices array
+matrices = [ #per tutte le linee
+                [ # e per turre le colonne
+                    ( #crea una tupla con dentro
+                        slice(x, x + data_shape[0]),
+                        slice(y, y + data_shape[1])
+                    )
+                for y in range(data_shape[1])
+                ]
+            for x in range(data_shape[0])
+            ]
+#precalcolo di ogni matrice
+matrices = [ #per tutte le linee
+                [ # e per turre le colonne
+                    ( #crea una tupla con dentro
+                        E_matrix[matrices[x][y]],
+                        P_matrix[matrices[x][y]]
+                    )
+                for y in range(data_shape[1])
+                ]
+            for x in range(data_shape[0])
+            ]
 
 print("Creating starting zero fields...")
 E_x = np.zeros(data_shape)
 E_y = np.zeros(data_shape) #all separate
 P = np.zeros(data_shape)
 
-print("Creating distance matrix...")
+print("Calculating potential...")
+for ix in range(data_shape[0]):
+    for iy in range(data_shape[1]):
+        P += matrices[ix][iy][1] * Charge[ix,iy]
 
 
-def sqr_dist_matrix(dimx, dimy):
-    print("    Creating duble base coordinate matrix...")
-    x = np.arange(-dimx, dimx + 1) #+1 is for safety.. i doubt its usefullness
-    y = np.arange(-dimy, dimy + 1)
-    X, Y = np.meshgrid(x, y)
-    return X ** 2 + Y ** 2
-
-inv_sqr_D_matrix = 1/(sqr_dist_matrix(*data_shape) * (scene.graph_setup.prec**2)) #distance, cell [datashape] is 0
-
-def blit(dest, src, loc): #blit function
-    pos = [i if i >= 0 else None for i in loc] #valori positivi
-    neg = [-i if i < 0 else None for i in loc] #valori negativi
-    target = dest[[slice(i,None) for i in pos]]  #prende la parte in cui copiare, tagliando dai positivi in su
-    src = src[[slice(i, j) for i,j in zip(neg, target.shape)]]  #taglia solo la parte da copiare
-    target[[slice(None, i) for i in src.shape]] = src #copia quella parte
-    return dest
-
+print("Showing off my result...")
 import plotly
 plotly.offline.plot(plotly.graph_objs.Data([
-    plotly.graph_objs.Contour(x=x, y=y, z=(Charge / (scene.graph_setup.prec**2) ),
+    plotly.graph_objs.Contour(x=x, y=y, z=(P),
                            contours=dict(
                                 coloring='heatmap'
                            ),
                            colorbar=dict(
-                                thickness=25,
-                                thicknessmode='pixels',
-                                len=0.5,
-                                lenmode='fraction',
-                                outlinewidth=0,
-                                ticksuffix = "C/m^2"
+                                ticksuffix = "C"
                             ))]), filename='TestFile.html')
 
 
